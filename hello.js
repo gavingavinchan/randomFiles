4f00a2ecd183e81b7697859e1a4f6299bfc62650
gavingavinchan <gavingavinchan@gmail.com>
2018-08-21 11:48:53 +0800

    testing with hello.js

--- hello.js
+++ hello.js
@@ -2,26 +2,44 @@
 var io1 = require('socket.io').listen(8321);
 
 io1.on('connection', function(socket1) {
+
+
+  socket1.use((packet, next) => {
+    io1.emit(packet[0], packet[1]);
+    return next();
+  });
+
+  socket1.use((packet, next) => {
+    console.log(new Date() + '\t' + packet[0] + '\t' + packet[1]);
+    return next();
+  });
+
+  console.log('connection');
   socket1.on('bar', function(msg1) {
-    console.log(msg1);
+    console.log('s' + msg1);
+    //io1.emit('sev', 'message from server!!!');
   });
 });
 
 // Mirror
-var ioIn = require('socket.io').listen(8123);
-var ioOut = require('socket.io-client');
-var socketOut = ioOut.connect('http://localhost:8321');
 
-
-ioIn.on('connection', function(socketIn) {
-  socketIn.on('foo', function(msg) {
-    socketOut.emit('bar', msg);
-  });
-});
 
 // Client
 var io2 = require('socket.io-client');
-var socket2 = io2.connect('http://localhost:8123');
+var socket2 = io2.connect('http://localhost:8321');
 
-var msg2 = "hello";
-socket2.emit('foo', msg2);
+var msg2 = "hello from 2";
+socket2.on('sev', function(msg){
+  // console.log('server' + msg);
+})
+
+socket2.emit('status', msg2);
+
+var socket3 = io2.connect('http://localhost:8321');
+
+var msg3 = "hello from 3";
+socket3.on('status', function(msg){
+  console.log('bar-3' + msg);
+})
+
+socket3.emit('thrust', msg3);
